3e0b568334d7c19344d5b8778b5d2be5aa414d08
Alexa Lewis <alexalewisdevelops@gmail.com>
2020-03-29 13:42:42 -0400

    getting api key

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Watch Tv
+
+Create a home page that has:
+the list of all "Top Rated" TV shows, returned from this API https://api.themoviedb.org/3/tv/top_rated?api_key=<<api_key>>&language=en-US&page=1
+
+This page should also highlight a random "Top Rated" TV show at the top of the page
+
+Create a /tv/:showId page that shows all the details for a given show and the cast of the show. The cast and crew end point is https://api.themoviedb.org/3/tv/{tv_id}/credits?api_key=<<api_key>>&language=en-US
+
+Deploy your site
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import TvShows from '../components/TvShows'
 
-const HomePage = () => {
-  return <HelloWorld />
+const HomePage = (props) => {
+  const [tvShows, setTvShows] = useState([])
+  const [featuredShow, setFeaturedShow] = useState([])
+
+  const getTopRated = async () => {
+    const resp = await axios.get(
+      'https://api.themoviedb.org/3/tv/top_rated?api_key=d823df9c8122b53033fc4aa49b428224&language=en-US&page=1'
+    )
+    setTvShows(resp.data.results)
+  }
+
+  useEffect(() => {
+    getTopRated()
+  }, [])
+
+  return (
+    <>
+      <header>
+        <h1>Top Rated Tv Shows</h1>
+      </header>
+
+      <main>
+        <h2>Featured of the Day</h2>
+        <ul>
+          {tvShows.map((tvShows) => {
+            return <TvShows id={tvShows.id} title={tvShows.title} />
+          })}
+        </ul>
+      </main>
+    </>
+  )
 }
 
 export default HomePage
